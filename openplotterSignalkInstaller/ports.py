7b1cf1dbaf858b28728fbb6b0d05cc6e755b1893
Sailoog <info@sailoog.com>
2019-09-07 18:29:55 +0200

    add sk connections to ports

--- openplotterSignalkInstaller/ports.py
+++ openplotterSignalkInstaller/ports.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Openplotter. If not, see <http://www.gnu.org/licenses/>.
-import os
+import os, ujson
 from openplotterSettings import platform
 from openplotterSettings import language
 
@@ -25,8 +25,22 @@ class Ports:
 		language.Language(currentdir,'openplotter-signalk-installer',currentLanguage)
 		self.platform2 = platform.Platform()
 		self.connections = []
-		self.connections.append({'id':'conn1', 'description':_('Signal K - Admin'), 'data':[], 'type':'TCP', 'mode':'server', 'address':'localhost', 'port':self.platform2.skPort, 'editable':'0'})
-		self.connections.append({'id':'conn2', 'description':_('Signal K - NMEA 0183 output'), 'data':[], 'type':'TCP', 'mode':'server', 'address':'localhost', 'port':'10110', 'editable':'0'})
+		self.connections.append({'id':'skConn1', 'description':_('Signal K - Admin'), 'data':[], 'type':'TCP', 'mode':'server', 'address':'localhost', 'port':self.platform2.skPort, 'editable':'0'})
+		self.connections.append({'id':'skConn2', 'description':_('Signal K - NMEA 0183 output'), 'data':[], 'type':'TCP', 'mode':'server', 'address':'localhost', 'port':'10110', 'editable':'0'})
 
 	def usedPorts(self):
-		if self.platform2.skPort: return self.connections
+		if self.platform2.skPort:
+			#try:
+			setting_file = self.platform2.skDir+'/settings.json'
+			data = ''
+			with open(setting_file) as data_file:
+				data = ujson.load(data_file)
+			if 'pipedProviders' in data:
+				for i in data['pipedProviders']:
+					if i['pipeElements'][0]['options']['subOptions']['type']=='udp':
+						self.connections.append({'id':i['id'], 'description':_('Signal K connection'), 'data':[], 'type':'UDP', 'mode':'server', 'address':'localhost', 'port':i['pipeElements'][0]['options']['subOptions']['port'], 'editable':'0'})
+					elif i['pipeElements'][0]['options']['subOptions']['type']=='tcp':
+						if i['pipeElements'][0]['options']['subOptions']['host']=='localhost' or i['pipeElements'][0]['options']['subOptions']['host']=='127.0.0.1':
+							self.connections.append({'id':i['id'], 'description':_('Signal K connection'), 'data':[], 'type':'TCP', 'mode':'client', 'address':'localhost', 'port':i['pipeElements'][0]['options']['subOptions']['port'], 'editable':'0'})
+			#except:pass
+			return self.connections
